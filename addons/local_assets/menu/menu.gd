2d79dba2e253df3cca4de2e12fee16959d4b553b
malakai <kaifungaming@proton.me>
2025-12-29 15:25:57 -0600

    added more default file names

--- addons/local_assets/menu/menu.gd
+++ addons/local_assets/menu/menu.gd
@@ -80,7 +80,7 @@ func set_up_settings():
 	if !editorSettings.has_setting("Local_Assets/File_preview_names"):
 		set_editor_setting(
 			"Local_Assets/File_preview_names",
-			PackedStringArray(["Preview", "Asset", "^(?i)preview.*", "^(?i)asset.*"]),
+			PackedStringArray(["Preview", "Asset", "^(?i)preview.*", "^(?i)asset.*","^(?i)content.*"]),
 			TYPE_PACKED_STRING_ARRAY
 		)
 	if !editorSettings.has_setting("Local_Assets/page_size"):
